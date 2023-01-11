57c10a2461b212801c1e837b839ff017cded9372
WyldeCat <skan1543@gmail.com>
2023-01-11 13:08:15 +0900

    Fix scripts

--- scripts/prepare_docker.sh
+++ scripts/prepare_docker.sh
@@ -1,8 +1,7 @@
-docker load -i /home/jeesoo/jira/rocm_pytorch
+docker load -i /home/jeesoo/jira/rocm_pytorch_modified
 docker pull rdma/container_tools_installer
 docker pull rdma/sriov-plugin
 
 docker run --net=host -v /usr/bin:/tmp rdma/container_tools_installer
-docker run -d -v /run/docker/plugins:/run/docker/plugins -v /etc/docker:/etc/docker -v /var/run:/var/run --net=host --privileged rdma/sriov-plugin
 
 docker network create -d sriov --subnet=192.169.16.0/24 -o netdevice=ib0 ibnet
